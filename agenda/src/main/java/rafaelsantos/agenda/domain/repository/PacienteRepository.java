11e803300f502e273e9bfc8100ebedcbb7713d3d
Rafael Santos <rafa.05125@gmail.com>
2023-03-02 10:49:05 -0300

    Paciente service, repository

---
+++ agenda/src/main/java/rafaelsantos/agenda/domain/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package rafaelsantos.agenda.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rafaelsantos.agenda.domain.entity.Paciente;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
